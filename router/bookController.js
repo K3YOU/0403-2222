34aed0a0b15b9b8e83c717f29947cdcc2a821802
K3YOU <7592667@naver.com>
2023-02-06 19:14:49 +0900

    0206

---
+++ router/bookController.js
@@ -0,0 +1,98 @@
+//판매량 높은 순으로 정렬하기
+function sortBookByBookSold(bookDB){
+
+    for(i=0;i<bookDB.length;i++){
+        var maxNo = bookDB[i]["bookSold"]
+        var maxIndex = i
+        
+        for(j=i;j<bookDB.length;j++){
+            if(maxNo<bookDB[j]["bookSold"]){
+                maxNo = bookDB[j]["bookSold"]
+                maxIndex = j
+            }
+        }
+        var temp = bookDB[i]
+        bookDB[i] = bookDB[maxIndex]
+        bookDB[maxIndex] = temp;
+    }
+}
+
+
+// 번호 순으로 오름차순 정렬하기 (전체보기)
+function sortBookByBookNo(bookDB){
+    for(i=0;i<bookDB.length;i++){
+        var minNo = bookDB[i]["bookNo"]
+        var minIndex = i
+
+        for(j= i;j<bookDB.length;j++){
+            if(minNo>bookDB[j]["bookNo"]){
+                minNo = bookDB[j]["bookNo"]
+                minIndex = j
+            }
+        
+        var temp = bookDB[i]
+        bookDB[i] = bookDB[minIndex]
+        bookDB[minIndex] = temp;
+        }
+    }
+}
+
+
+
+module.exports = function(app){
+
+    // main페이지
+    app.get("/mainBook", function(req, res){ 
+        var log = req.session.log;
+        var bookDB = req.session.bookDB;
+        var name = req.session.name;
+
+        sortBookByBookSold(bookDB);
+
+        var renderData = {	
+            "log" : log,
+            "name" : name,
+            "bookDB" : bookDB
+        };
+        res.render("book/bookMain.ejs", renderData); 
+    });
+
+
+    //전체도서 보기 페이지(bookAllList)
+    app.get("/bookAllList",function(req,res){
+        //필요한 변수 지정 --> 저 페이지에 있어야할 것들이 무엇인가?
+        var log = req.session.log     // 로그인을 한 경우 로그인이 되었다고 알려줘야하므로
+        var bookDB = req.session.bookDB
+        var name = req.session.name
+       
+        //오름차순 정렬함수(bookDB)
+        sortBookByBookNo(bookDB)
+
+        //렌더링에 필요한 변수들 지정
+        var renderData= {
+            'log' : log,
+            'bookDB' : bookDB,
+            'name' :name
+        }
+
+        res.render("book/bookAllList.ejs",renderData)
+    });
+
+    //OS 
+    app.get("/bookOSList",function(req,res){
+        var log = req.session.log
+        var bookDB = req.session.bookDB
+        var name = req.session.name
+
+        //os인 책만 정렬하는 함수
+        sortBookByBookSold(bookDB)
+
+        //렌더링에 필요한 변수들 지정
+        var renderData={
+            'log' : log,
+            'bookDB' : bookDB,
+            'name' :name
+        }
+        res.render("book/bookOSList.ejs",renderData)
+    })
+}
